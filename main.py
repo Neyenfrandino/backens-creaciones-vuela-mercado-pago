a6e5e885eafafb1d897576eaec2fe7139d678dd6
Neyen Frandino <neyenfrandino1@gmail.com>
2024-12-18 10:46:23 -0300

    entorno de produccion

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
-import os  # Para leer la variable de entorno
-
+from starlette.responses import JSONResponse
 from app.routers import mercado_pago
 
 app = FastAPI()
 
+# Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://tienda-web-creaciones-vuela.netlify.app"],
+    allow_origins=[
+        "http://localhost:3000", 
+        "https://tienda-web-creaciones-vuela.netlify.app"
+    ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
     allow_headers=["*"],
 )
 
+# Incluir las rutas de mercado_pago
 app.include_router(mercado_pago.router)
 
+# Manejo explícito de solicitudes OPTIONS
+@app.options("/{any_path:path}")
+async def handle_options(any_path: str):
+    return JSONResponse(status_code=200)
+
 if __name__ == "__main__":
-    # Usar el puerto proporcionado por el entorno, o 8000 si no está configurado
-    port = int(os.getenv("PORT", 8000))
-    
-    uvicorn.run('main:app', host="0.0.0.0", port=port, reload=True)
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
