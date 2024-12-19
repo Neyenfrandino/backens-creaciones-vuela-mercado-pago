2d53a541d604047830a121ea5de3a10efbd0c91a
Neyen Frandino <neyenfrandino1@gmail.com>
2024-12-19 19:49:40 -0300

    buscando el bug

--- app/repository/mercado_pago_repo.py
+++ app/repository/mercado_pago_repo.py
@@ -1,31 +1,33 @@
 import httpx
 from fastapi import HTTPException
 
-def mercadopago_repository(schema: dict, sdk):
-    try:
-        # Validación del esquema
-        if not isinstance(schema, dict):
-            raise ValueError("El esquema debe ser un diccionario válido.")
+def mercadopago_repository(schema, sdk):
+    print(schema, 'schema')
+    print(sdk, 'sdk')
+    # try:
+    #     # Validación del esquema
+    #     if not isinstance(schema, dict):
+    #         raise ValueError("El esquema debe ser un diccionario válido.")
 
-        # Crear preferencia usando el SDK
-        response = sdk.preference().create(schema)
+    #     # Crear preferencia usando el SDK
+    #     response = sdk.preference().create(schema)
 
-        # Validar respuesta de la API
-        if response.get("status") in [200, 201]:  # Estados HTTP de éxito
-            print("Preferencia creada exitosamente:", response.get("response"))
-            return response.get("response")
-        else:
-            error_message = f"Error al crear preferencia. Código: {response.get('status')}, Detalles: {response.get('response')}"
-            print(error_message)
-            raise Exception(error_message)
+    #     # Validar respuesta de la API
+    #     if response.get("status") in [200, 201]:  # Estados HTTP de éxito
+    #         print("Preferencia creada exitosamente:", response.get("response"))
+    #         return response.get("response")
+    #     else:
+    #         error_message = f"Error al crear preferencia. Código: {response.get('status')}, Detalles: {response.get('response')}"
+    #         print(error_message)
+    #         raise Exception(error_message)
 
-    except ValueError as ve:
-        print(f"Error de validación: {ve}")
-        raise ve  # Re-lanzar para que el controlador lo maneje si es necesario
+    # except ValueError as ve:
+    #     print(f"Error de validación: {ve}")
+    #     raise ve  # Re-lanzar para que el controlador lo maneje si es necesario
 
-    except Exception as e:
-        print(f"Error inesperado al crear preferencia: {str(e)}")
-        raise e  # Re-lanzar para mantener el contexto del error
+    # except Exception as e:
+    #     print(f"Error inesperado al crear preferencia: {str(e)}")
+    #     raise e  # Re-lanzar para mantener el contexto del error
 
 # Función para confirmar el pago en MercadoPago
 async def confirm_payment(payment_id, access_token):
